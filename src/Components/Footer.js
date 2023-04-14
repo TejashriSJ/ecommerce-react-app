b90b47872a5f8ca3e5ffb680272375abc037be0a
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-14 14:59:05 +0530

    Add footer js file

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <div className="footer-navigations">
+          <p>About Us</p>
+          <p>Contact Us</p>
+        </div>
+        <p className="copyRights">© All Rights Reserved</p>
+      </div>
+    );
+  }
+}
+export default Footer;
