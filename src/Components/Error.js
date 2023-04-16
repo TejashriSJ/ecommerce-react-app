01a1a3b771aa8ded3069e125398da9b7ae83bcce
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-16 12:57:32 +0530

    Update Errors file to use hooks

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 
-class Error extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Error In Loading the Page.</h1>
-      </div>
-    );
-  }
+function Error() {
+  return (
+    <div>
+      <h1>Error In Loading the Page.</h1>
+    </div>
+  );
 }
 export default Error;
